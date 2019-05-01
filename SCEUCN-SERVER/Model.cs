321c71d94253f640fc91ba71e308c2d80c2f5dcd
Christian Farias <kurisu615@gmail.com>
2019-05-01 13:03:42 -0400

    Added Vehiculo, Logo Added TipoVehiculo, TipoLogo, Planta

--- SCEUCN-SERVER/Model.cs
+++ SCEUCN-SERVER/Model.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 
 namespace SCEUCN_SERVER.Model
 {
-
     public class DatabaseContext : DbContext
     {
 
@@ -11,15 +10,44 @@ namespace SCEUCN_SERVER.Model
         public DbSet<Persona> Personas { get; set; }
     }
 
+    public enum TipoVehiculo { Auto, Camioneta, Moto }
+    
+    public enum TipoLogo {Funcionario, Estudiante}
+    
+    public enum Planta {Oficial, Fija, Temporal}
 
     public class Persona
     {
+        
         public int Id { get; set; }
+        
         public string Rut { get; set; }
 
         public string Nombres { get; set; }
-
+        
         public string Apellidos { get; set; }
+    }
+
+    public class Vehiculo
+    {
+
+        public Persona persona;
+        
+        public string patente;
+        
+        public string marca;
+        
+        public TipoVehiculo tipo;
+    }
+
+    public class Logo
+    {
+
+        public string id;
+        
+        public string fecha;
+        
+        public TipoLogo tipo;
     }
 
 }
