99a570ad040cabc68330b387862c688ec2036108
Diego Urrutia Astorga <mika.cl@gmail.com>
2019-05-22 16:47:41 -0400

    Added: Java version of server.

---
+++ SCEUCN-SERVERj/src/main/java/cl/ucn/disc/pdis/sceucn/App.java
@@ -0,0 +1,21 @@
+/**
+ * (c) 2019 Projecto Desarrollo e Integracion de Soluciones.
+ */
+package cl.ucn.disc.pdis.sceucn;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Main Application
+ */
+@Slf4j
+public class App {
+    
+    /**
+     * Entry point!
+     */
+    public static void main(String[] args) {
+        log.debug("Starting ..");
+    }
+    
+}
---
+++ SCEUCN-SERVERj/src/test/java/cl/ucn/disc/pdis/sceucn/AppTest.java
@@ -0,0 +1,13 @@
+package cl.ucn.disc.pdis.sceucn;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    
+    @Test
+    public void testAppHasAGreeting() {
+        App classUnderTest = new App();
+        // assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+}
