973e1dbf8418ef9618589f1d3df7a52da3009740
Christian Farias <kurisu615@gmail.com>
2019-07-16 13:43:46 -0400

    Changed la manera de implementar las razor pages. Changed vista de la pagina principal.

--- SCEUCN-WEB/Pages/Vehiculos/Index.cshtml.cs
+++ SCEUCN-WEB/Pages/Vehiculos/Index.cshtml.cs
@@ -7,10 +7,10 @@ using CL.UCN.DISC.PDIS.SCE.Web.Controller;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CL.UCN.DISC.PDIS.SCE.Web.Pages {
+namespace CL.UCN.DISC.PDIS.SCE.Web.Pages.Vehiculos {
 
-    public class VehiculosModel : PageModel {
-
+    public class IndexModel : PageModel
+    {
         /// <summary>
         /// La instancia de la aplicacion de Ice.
         /// </summary>
@@ -20,26 +20,18 @@ namespace CL.UCN.DISC.PDIS.SCE.Web.Pages {
         /// Constructor.
         /// </summary>
         /// <param name="IceApplication"></param>
-        public VehiculosModel([FromServices] IWebController webController) => this.webController = webController;
+        public IndexModel([FromServices] IWebController webController) => this.webController = webController;
 
         /// <summary>
         /// La lista de vehiculos.
         /// </summary>
         /// <typeparam name="Vehiculo"></typeparam>
         /// <returns></returns>
-        public List<Vehiculo> Vehiculos { get; set; } = new List<Vehiculo>();
+        public IList<Vehiculo> Vehiculos { get; set; } = new List<Vehiculo>();
 
-
-        /// <summary>
-        /// La lista de personas.
-        /// </summary>
-        /// <typeparam name="Persona"></typeparam>
-        /// <returns></returns>
-        public List<Persona> Personas { get; set; } = new List<Persona>();
 
         public void OnGet() {
             Vehiculos = webController.GetVehiculos();
-            Personas = webController.GetPersonas();
         }
     }
 }
